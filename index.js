80aa990dd0705553178ff26ec1f85ea774046c59
hideki <viethungpro97@gmail.com>
2018-12-23 07:09:05 +0700

    add room

--- index.js
+++ index.js
@@ -5,17 +5,35 @@ const PORT = process.env.PORT || 3000
 const io = require('socket.io')(server)
 
 let names = []
+let rooms = []
 let msgs = []
 app.use(express.static('public'))
 app.set('view engine', 'pug')
 app.set('views', './views')
+
+const updateRoomMember = roomName => {
+    let names = []
+    if(io.sockets.adapter.rooms[`${roomName}`])
+    // console.log(io.sockets.adapter.rooms[`${roomName}`])
+        for(id in io.sockets.adapter.rooms[`${roomName}`].sockets) {
+        {
+            names.push(io.sockets.sockets[`${id}`].name)
+        }
+    }
+    return names
+}
 
 io.on('connection', socket=> {
     socket.on('disconnect', ()=> {
         let index = names.indexOf(socket.name)
         if(index !== -1) {
             names.splice(index, 1)
-            io.sockets.emit('online-list', names)            
+        }
+        index = rooms.findIndex(room=> room.name === socket.roomName)
+        if(index!==-1) {
+            rooms[index].number = (socket.adapter.rooms[`${socket.roomName}`].length)
+            rooms[index].members = updateRoomMember(socket.roomName)
+            io.sockets.in(socket.roomName).emit('online-list', rooms[index].members)
         }
     })
     socket.on('new-name', name=> {
@@ -23,21 +41,47 @@ io.on('connection', socket=> {
             names.push(name)
             socket.name = name
             socket.emit('accept')
-            io.sockets.emit('online-list', names)
+            io.sockets.emit('rooms', rooms)
         } else {
             socket.emit('refuse')
         }
     })
+    socket.on('create-room', roomName=> {
+        let isExisted = rooms.some(room=>{
+            return room.name === roomName
+        })
+        if(!isExisted) {
+            socket.join(roomName)
+            socket.roomName = roomName
+            let newRoom = {name: roomName, owner: socket.name, number: 1, members: [socket.name]}
+            rooms.push(newRoom)
+            io.sockets.emit('rooms', rooms)
+            socket.emit('enter-room')
+            io.sockets.in(roomName).emit('online-list', newRoom.members)
+        } else io.sockets.emit('room-existed')
+    })
+    socket.on('join-room', roomName=> {
+        let index = rooms.findIndex(room=> room.name === roomName)
+        if(index!==-1) {
+            socket.join(roomName)
+            socket.roomName = roomName
+            rooms[index].number = (socket.adapter.rooms[`${roomName}`].length)
+            rooms[index].members = updateRoomMember(roomName)
+            io.sockets.emit('rooms', rooms)
+            socket.emit('enter-room')
+            io.sockets.in(roomName).emit('online-list', rooms[index].members)
+        }
+    }) 
     socket.on('is-typing', ()=> {
-        socket.broadcast.emit('person-is-typing', socket.name)
+        socket.in(socket.roomName).broadcast.emit('person-is-typing', socket.name)
     })
     socket.on('was-leaved', ()=> {
-        socket.broadcast.emit('person-was-leaved', socket.name)
+        socket.in(socket.roomName).broadcast.emit('person-was-leaved', socket.name)
     })
     socket.on('chat', msg=> {
         let newchat = {name: socket.name, msg}
         msgs.push(newchat)
-        io.sockets.emit('new-msg', newchat)
+        io.sockets.in(socket.roomName).emit('new-msg', newchat)
     })
 })
 
